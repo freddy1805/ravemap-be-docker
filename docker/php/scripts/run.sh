636d1f3ad078cc063ee93fffe1ca14d4f3867673
Frederik Röttgerkamp <frederik.roettgerkamp@q-two.net>
2021-09-15 16:00:47 +0200

    Anpassungen

--- docker/php/scripts/run.sh
+++ docker/php/scripts/run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 echo "start downloading backend software..."
 cd /var/www
-git clone https://freddy1805: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@github.com/freddy1805/ravemap-backend.git symfony/
+FILE=/var/www/symfony
+if [ -d "$FILE" ]; then
+    echo "$FILE exists. nothing to do!"
+else 
+    echo "$FILE does not exist. starting installation!"
+    git clone https://freddy1805:Imbusch49@github.com/freddy1805/ravemap-backend.git symfony/
+fi
+
 
 echo "running post install scripts for php..."
 cd /var/www/symfony
@@ -14,14 +21,14 @@ if [ -d "$FILE" ]; then
 else 
     echo "$FILE does not exist. starting composer!"
     composer install
+
+    echo "initialize db structure..."
+    php bin/console doctrine:schema:create --force
+
+    echo "create admin account and grant rights... (username: admin | password: test1234)"
+    php bin/console fos:user:create admin info@ravemap.tk test1234
+    php bin/console fos:user:promote admin ROLE_ADMIN
 fi
 
-echo "initialize db structure..."
-php bin/console doctrine:schema:create --force
-
-
-echo "create admin account and grant rights... (username: admin | password: test1234)"
-php bin/console fos:user:create admin info@ravemap.tk test1234
-php bin/console fos:user:promote admin ROLE_ADMIN
 
 php-fpm
