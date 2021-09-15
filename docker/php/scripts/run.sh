ba54a6b454a0f8c5fb61d2932eb657fb7b444ff6
Frederik Röttgerkamp <frederik.roettgerkamp@q-two.net>
2021-09-15 15:56:34 +0200

    Anpassungen

--- docker/php/scripts/run.sh
+++ docker/php/scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "start downloading backend software..."
 cd /var/www
-git clone https://github.com/freddy1805/ravemap-backend.git > symfony
+git clone https://github.com/freddy1805/ravemap-backend.git symfony/
 
 echo "running post install scripts for php..."
 cd /var/www/symfony
