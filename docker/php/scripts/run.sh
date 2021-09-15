76561d8083f2b24264d3941a9360629f1ab8cb02
Frederik Röttgerkamp <frederik.roettgerkamp@q-two.net>
2021-09-15 15:08:40 +0200

    updated docker

--- docker/php/scripts/run.sh
+++ docker/php/scripts/run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+echo "start downloading backend software..."
+cd /var/www
+git clone https://github.com/freddy1805/ravemap-backend.git > symfony
 
-echo "running post install scripts for php..";
+echo "running post install scripts for php..."
 cd /var/www/symfony
 
-echo "CHECK COMPOSER DEPENDENCIES";
+
+echo "running composer..."
 FILE=/var/www/symfony/vendor
 if [ -d "$FILE" ]; then
     echo "$FILE exists. nothing to do!"
@@ -11,5 +15,13 @@ else
     echo "$FILE does not exist. starting composer!"
     composer install
 fi
+
+echo "initialize db structure..."
+php bin/console doctrine:schema:create --force
+
+
+echo "create admin account and grant rights... (username: admin | password: test1234)"
+php bin/console fos:user:create admin info@ravemap.tk test1234
+php bin/console fos:user:promote admin ROLE_ADMIN
 
 php-fpm
--- ravemap/README.md
+++
@@ -1,1 +0,0 @@
-# ravemap-backend
